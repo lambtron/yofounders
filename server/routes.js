dff4025bbcfff6c7317b34cae95dda74d977df28
Andy Jiang <andyjiang@gmail.com>
2014-11-22 22:30:50 -0800

    fixed bitly link

--- server/routes.js
+++ server/routes.js
@@ -32,8 +32,7 @@ Routes.getYo = function *getYo() {
   var lat = location.substring(0, location.indexOf(';'));
   var lng = location.substring(location.indexOf(';') + 1);
   var link = yield Founders.get(lat, lng);
-  console.log(link);
-  Yo.yo_link(username, link);
+  Yo.yo_link(username, link.url);
   this.body = 'OK';
 };
 
